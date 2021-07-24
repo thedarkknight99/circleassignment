42d2f18f66cdd6624396c8ca4c4d4879275e8a28
Abhishek <66775793+thedarkknight99@users.noreply.github.com>
2021-07-24 17:05:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,24 @@ function clickHandler() {
     var side = document.getElementById("side").value;
     var radius = document.getElementById("radius").value;
 
-    //e.preventDefault();
+    
     let num =   Math.floor(side/(2*radius)); 
-    //console.log(num)
-    //document.getElementById("top").innerHTML = num;
+   
     var mainelement = document.createElement("div");
-    mainelement.setAttribute("style", `border: 1px solid; height:${side}; width:${side}`);
-    mainelement.innerHTML = "aaaa";
+    mainelement.setAttribute("style", `border: 1px solid; height:${side}px; width:${side}px`);
+    mainelement.innerHTML = "square";
     document.body.appendChild(mainelement);
     
 
     for(var i=0; i < num; i++) {
         for(var j=0; j < num; j++){
             var element = document.createElement("div");
-            element.innerHTML = "1234"
-            element.setAttribute("style", "color: blue; border:1px solid; border-radius: 50%; background-color: orange; height: 100px; width:100px; display:inline");
+            element.innerHTML = "circle"
+            element.setAttribute("style", `border:1px solid; border-radius: 50%; background-color: orange; height:${radius}px; width:${radius}px; display:inline`);
             document.body.appendChild(element);
         }
         var element1 = document.createElement("br");
-       
+        element1.setAttribute("style", "padding:10px; margin:10px")
         document.body.appendChild(element1);
     } 
     
