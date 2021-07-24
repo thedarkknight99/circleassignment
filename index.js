633298c5e00c594511f45b8d7185f44af759c31c
Abhishek <iotsummerschool2020@gmail.com>
2021-07-24 15:13:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function clickHandler() {
+
+    var top = document.getElementById("top").value;
+    var bottom = document.getElementById("bottom").value;
+    var side = document.getElementById("side").value;
+    var radius = document.getElementById("radius").value;
+
+    //e.preventDefault();
+    let num =   Math.floor(side/(2*radius)); 
+    //console.log(num)
+    //document.getElementById("top").innerHTML = num;
+    var mainelement = document.createElement("div");
+    mainelement.setAttribute("style", "border: 1px solid; height:300px; width:300px");
+    mainelement.innerHTML = "aaaa";
+    document.body.appendChild(mainelement);
+    
+
+    for(var i=0; i < 3; i++) {
+
+        for(var j=0; j < 3; j++){
+            var element = document.createElement("div");
+            element.innerHTML = "1234"
+            element.setAttribute("style", "color: blue; border:1px solid; border-radius: 50%; background-color: orange; height: 100px;width: 100px;display:inline");
+            document.body.appendChild(element);
+        }
+        var element1 = document.createElement("br");
+        document.body.appendChild(element1);
+    } 
+    
+}
+
